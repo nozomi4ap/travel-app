37a1485051a2ab94cd01bad978d8ab1c7ca47c30
nozomi4ap <nozomi4ap@gmail.com>
2026-09-17 01:53:47 +0900

    Add Firebase Storage initialization

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,11 @@
+
+
+
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
+// ▼▼▼ ここを、Firebaseコンソールで発行したご自身の設定に置き換えてください ▼▼▼
 const firebaseConfig = {
   apiKey: "AIzaSyBfGHOevefAsO9FsyCPp3vKw2T5rtM3RJA",
   authDomain: "travel-app-5b13a.firebaseapp.com",
@@ -9,6 +14,8 @@ const firebaseConfig = {
   messagingSenderId: "158547103359",
   appId: "1:158547103359:web:a9006762e444b44dddf94a",
 }
+// ▲▲▲ ここまで ▲▲▲
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const storage = getStorage(app)
